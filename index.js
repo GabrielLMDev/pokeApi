24cca5a45abb949f83651746ef23983f1e686eba
GabrielLMDev <gabriellm.contacto@gmail.com>
2023-12-23 22:54:34 -0600

    1.1

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
 const search = document.getElementById("searchPokemon");
-const random = document.getElementById("randomPokemon")
+const random = document.getElementById("randomPokemon");
 const closeM = document.getElementById("cerrarModal");
 const showM = document.getElementById("mostrarModal");
 const modal = document.getElementById("infoPokemon");
 
 search.addEventListener("click", function () {
-  var textPokemon = document.getElementById('idPokemon').value;
-  fetch('https://pokeapi.co/api/v2/pokemon/' + textPokemon)
-    .then(response => {
+  var textPokemon = document.getElementById("idPokemon").value;
+  fetch("https://pokeapi.co/api/v2/pokemon/" + textPokemon)
+    .then((response) => {
       if (!response.ok) {
-        alert('Sin resultados');
+        alert("Sin resultados");
       }
       return response.json();
     })
-    .then(data => {
-      let imagenPokemon = document.getElementById('imgPokemon');
+    .then((data) => {
+      let imagenPokemon = document.getElementById("imgPokemon");
       imagenPokemon.src = data.sprites.front_default;
 
-      let nombrePokemon = document.getElementById('nombre');
+      let nombrePokemon = document.getElementById("nombre");
       nombrePokemon.innerHTML = data.name;
 
-      let nombreP = document.getElementById('NombreP');
+      let nombreP = document.getElementById("NombreP");
       nombreP.innerHTML = data.name;
 
-      let tipoPokemon = document.getElementById('TipoP');
+      let tipoPokemon = document.getElementById("TipoP");
       tipoPokemon.innerHTML = data.types[0].type.name;
 
-      let habilidadPokemon = document.getElementById('HabilidadP');
+      let habilidadPokemon = document.getElementById("HabilidadP");
       habilidadPokemon.innerHTML = data.abilities[0].ability.name;
 
-      let alturaPokemon = document.getElementById('AlturaP');
+      let alturaPokemon = document.getElementById("AlturaP");
       alturaPokemon.innerHTML = data.height;
 
-      let pesoPokemon = document.getElementById('PesoP');
+      let pesoPokemon = document.getElementById("PesoP");
       pesoPokemon.innerHTML = data.weight;
     })
-    .catch(error => {
-      console.error('Error al obtener los datos:', error);
+    .catch((error) => {
+      console.error("Error al obtener los datos:", error);
     });
 });
 
@@ -49,9 +49,9 @@ showM.addEventListener("click", function () {
 });
 
 random.addEventListener("click", function () {
-  var textPokemon = document.getElementById('idPokemon');
+  var textPokemon = document.getElementById("idPokemon");
   textPokemon.value = randomNumber();
-})
+});
 
 function showModal() {
   modal.style.display = "block";
